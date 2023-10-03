da96dd921a2c5b04e6f12fc015718a6cb8d92857
fosterion <32929118+fosterion@users.noreply.github.com>
2023-10-03 22:58:11 +0200

    add generic repository interface

---
+++ src/Mkeeper.Core/Repositories/Abstract/IRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mkeeper.Core.Repositories.Abstract;
+
+public interface IRepository<T>
+{
+    IEnumerable<T> GetAll();
+    T GetById(int id);
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
